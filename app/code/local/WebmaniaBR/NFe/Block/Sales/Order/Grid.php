928c0f57481f8c290ea1ba5bdedde050f4552b7a
Equipe WebmaniaBR® <suporte@webmaniabr.com>
2016-04-13 18:03:16 -0300

    v 1.0.2
    Em algumas versões do Magento não estava chamando a função para emitir
    a NF-e de forma manual. O problema está corrigido e funcionando em
    todas as versões.

--- app/code/local/WebmaniaBR/NFe/Block/Sales/Order/Grid.php
+++ app/code/local/WebmaniaBR/NFe/Block/Sales/Order/Grid.php
@@ -1,4 +1,4 @@
-<?php
+<?
 class WebmaniaBR_NFe_Block_Sales_Order_Grid extends Mage_Adminhtml_Block_Sales_Order_Grid
 {   
     protected function _prepareMassaction()
@@ -8,7 +8,7 @@ class WebmaniaBR_NFe_Block_Sales_Order_Grid extends Mage_Adminhtml_Block_Sales_O
         $this->getMassactionBlock()->addItem(
             'nfe',
             array('label' => $this->__('Emitir NF-e'), 
-                  'url'   => $this->getUrl('nfe/standard/emitir') //this should be the url where there will be mass operation
+                  'url'   => Mage::helper("adminhtml")->getUrl("nfe/adminhtml_index/index/") //this should be the url where there will be mass operation
             )
         );
         
---
+++ app/code/local/WebmaniaBR/NFe/controllers/Adminhtml/IndexController.php
@@ -0,0 +1,48 @@
+<?
+
+class WebmaniaBR_Nfe_Adminhtml_IndexController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction(){
+        
+        $orders = $_POST['order_ids'];
+        
+        if (!is_array($orders)){
+            
+            if ($orders) $orders  = explode(',', $orders);
+            
+        }
+        
+        if ($orders && count($orders) > 0){
+            
+            foreach ($orders as $number){
+            
+                $order = Mage::getModel('sales/order')->load($number);
+
+                // Emissão automática de Nota Fiscal 
+                $notafiscal = new WebmaniaBR_NFe_Model_Observer;
+                $response = $notafiscal->emitirNfe( $order, null, null, true );
+                $orderno = (int) $order->getIncrementId();
+                if ($response->error) { 
+                    Mage::getSingleton('core/session')->addError("Nota Fiscal #".$orderno.': '.$response->error); 
+                    if ($response->log){
+                        foreach ($response->log as $erros){
+                            foreach ($erros as $erro) {
+                                Mage::getSingleton('core/session')->addError("- ".$erro);
+                            }
+                        }
+                    }
+                } else {
+                    Mage::getSingleton('core/session')->addSuccess("Nota Fiscal #".$orderno.': Emitida com sucesso.');
+                }
+
+            }
+        
+        }
+        
+        session_write_close();
+        $url = Mage::helper('adminhtml')->getUrl('adminhtml/sales_order/');
+        $this->_redirectUrl($url);
+
+    }
+    
+}
